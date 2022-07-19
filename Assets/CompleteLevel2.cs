535a2362b3248dac81832d8759db75866a4d931d
Hamza Masood <hamzam21@iitk.ac.in>
2022-07-20 03:53:28 +0530

    Level 3 Button added and level 2 ending fixed

--- Assets/CompleteLevel2.cs
+++ Assets/CompleteLevel2.cs
@@ -6,7 +6,16 @@ public class CompleteLevel2 : MonoBehaviour
 {
     public string menuSceneName = "MainMenu";
 
+    public string nextLevel = "Level03";
+    public int levelToUnlock = 3;
+
     public SceneFader sceneFader;
+
+    public void Continue()
+    {
+        PlayerPrefs.SetInt("levelReached", levelToUnlock);
+        sceneFader.FadeTo(nextLevel);
+    }
 
     public void Menu()
     {
